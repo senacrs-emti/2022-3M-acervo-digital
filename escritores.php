7f4bb3d918a4f4ff47b247cf4ee94cf8af0802b1
arthurmdondoni <103116523+arthurmdondoni@users.noreply.github.com>
2022-06-06 10:32:34 -0300

    .

--- escritores.php
+++ escritores.php
@@ -46,24 +46,30 @@
 <!--"Fim do menu"-->
 
 <!--Sobre-->
-<div class="container-fluid-1">
-<div class="row">
-<H2>Ola Mundo</H2>
-<div class="col">
-  <img src="image/Massis.png" alt="">
+<div class="container">
+<div class="row align-items-center">
+  
+    <div class="col">
+    <h2>Cruz e Souza</h2>
+<h5>(1861-1891)</h5>
 
-
-
+<p>Cruz e Sousa, principal nome do Simbolismo brasileiro, nasceu em 24 de novembro de 1861. Filho de escravos alforriados, teve acesso à educação formal graças ao apadrinhamento do ex-“dono” de sua mãe. Durante toda a sua vida, sofreu com o preconceito racial, mas lutou contra a escravidão e o racismo. Para sobreviver, trabalhou na Companhia Dramática Julieta dos Santos e, mais tarde, na Estrada de Ferro Central do Brasil,
+   quando adquiriu tuberculose, o que o levou à morte em 19 de março de 1898.</p>
+    </div>
+    <div class="col">
+      
+<img src="image/souza.png" alt="">
+    </div>
+  </div>
+    
 
 
 
 </div>
-</div>
-</div>
-<!--Fims sobre-->
+<!--Fim sobre-->
 
 <!--List-->
-<div class="container-fluid-2">
+
 <div class="accordion accordion-flush" id="accordionFlushExample">
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingOne">
