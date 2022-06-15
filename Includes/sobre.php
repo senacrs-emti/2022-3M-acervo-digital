07603b5d519d6d6f15906912c0f23d39ae45ee8c
arthurmdondoni <103116523+arthurmdondoni@users.noreply.github.com>
2022-06-15 09:26:13 -0300

    includes

---
+++ Includes/sobre.php
@@ -0,0 +1,15 @@
+
+<!--Sobre-->
+<div class="container">
+<div class="row align-items-center">
+    <div class="col ">
+
+    <h2>Cruz e Souza</h2>
+<h5>(1861-1891)</h5>
+<img src="image/souza.png">
+<p>Cruz e Sousa, principal nome do Simbolismo brasileiro, nasceu em 24 de novembro de 1861. Filho de escravos alforriados, teve acesso à educação formal graças ao apadrinhamento do ex-“dono” de sua mãe. Durante toda a sua vida, sofreu com o preconceito racial, mas lutou contra a escravidão e o racismo. Para sobreviver, trabalhou na Companhia Dramática Julieta dos Santos e, mais tarde, na Estrada de Ferro Central do Brasil,
+   quando adquiriu tuberculose, o que o levou à morte em 19 de março de 1898.</p>
+    </div>
+  </div>
+</div>
+<!--Fim sobre-->
--- escritores.php
+++ escritores.php
@@ -1,66 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <!--Estilo bootstrap-->
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-  <!---->
-<link rel="stylesheet" href="style2.css">
-    <title>Escritores</title>
+<?php 
 
-</head>
-<header>
-  <!--Menu-->
- <nav class="navbar navbar-light bg-light fixed-top">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Dondoni</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
-      <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Dondoni</h5>
-        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-      </div>
-      <div class="offcanvas-body">
-        <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          
-        <form class="d-flex">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </div>
-</nav>
-<!--"Fim do menu"-->
-</header>
+include_once "includes/head.php";
+include_once "includes/menu.php";
+?>
 
 <body>
  
 
-<!--Sobre-->
-<div class="container">
-<div class="row align-items-center">
-    <div class="col ">
-
-    <h2>Cruz e Souza</h2>
-<h5>(1861-1891)</h5>
-<img src="image/souza.png">
-<p>Cruz e Sousa, principal nome do Simbolismo brasileiro, nasceu em 24 de novembro de 1861. Filho de escravos alforriados, teve acesso à educação formal graças ao apadrinhamento do ex-“dono” de sua mãe. Durante toda a sua vida, sofreu com o preconceito racial, mas lutou contra a escravidão e o racismo. Para sobreviver, trabalhou na Companhia Dramática Julieta dos Santos e, mais tarde, na Estrada de Ferro Central do Brasil,
-   quando adquiriu tuberculose, o que o levou à morte em 19 de março de 1898.</p>
-    </div>
-  </div>
-</div>
-<!--Fim sobre-->
 
 <!--List-->
 
