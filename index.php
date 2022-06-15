61abd7d20967e0f85429bf776506a9c5ffebd82b
arthurmdondoni <103116523+arthurmdondoni@users.noreply.github.com>
2022-06-15 09:12:22 -0300

    .

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="estilo.css">
   
   <!--Estilo bootstrap-->
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
@@ -27,7 +27,7 @@
       <div class="offcanvas-body">
         <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="souza.php">Home</a>
+            <a class="nav-link active" aria-current="page" href="escritores.php">Home</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="#">Link</a>
@@ -58,6 +58,7 @@
     </header>
 
 <body>
+
 <!--Carossel
 <div class="container">
   <div class="row">
@@ -72,7 +73,7 @@
       <img src="image/livro.png" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src=ef..." class="d-block w-100" alt="...">
     </div>
   </div>
 </div>
@@ -84,22 +85,45 @@
 
 <!--cardi-->
 <div class="container">
-<div class="row align-items-center">
+<div class="row row-cols-1 row-cols-md-2 g-4">
   <div class="col">
-
-  <div class="card" style="width: 15rem;">
-  <img src= "image/souza.png" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">Cruz e Souza</h5>
-    <p class="card-text">O escritor brasileiro Cruz e Sousa nasceu em 1861 e faleceu em 1898.</p>
-    <a href="souza.php" class="btn btn-success">Confira</a>
-
+    <div class="card">
+      <img src="image/cruz.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Cruz e Souza</h5>
+        <p class="card-text">João da Cruz e Sousa (Nossa Senhora do Desterro, 24 de novembro de 1861 — Curral Novo, 19 de março de 1898) foi um poeta brasileiro. Com a alcunha de Dante Negro ou Cisne Negro, foi um dos principais representantes do simbolismo no Brasil.</p>
+     <a href="escritores.php"> Confira </a>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="..." class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+    </div>
   </div>
 </div>
-</div>
-</div>  
-</div>
-
 
 <!--FIM CARDI-->
 
