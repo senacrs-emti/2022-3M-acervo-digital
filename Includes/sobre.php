df032a9af58fb6b62c8ec09d6ea5f797c0ee7518
gh0stumb <103116523+arthurmdondoni@users.noreply.github.com>
2022-06-20 10:01:23 -0300

    ,

--- Includes/sobre.php
+++ Includes/sobre.php
@@ -9,7 +9,7 @@
 
     <div class="col ">
 
-    <h2>Cruz e Sousa</h2>
+    <h2></h2>
 <h5>(1861-1891)</h5>
 
 <p>Cruz e Sousa, principal nome do Simbolismo brasileiro, nasceu em 24 de novembro de 1861. Filho de escravos alforriados, teve acesso à educação formal graças ao apadrinhamento do ex-“dono” de sua mãe. Durante toda a sua vida, sofreu com o preconceito racial, mas lutou contra a escravidão e o racismo. Para sobreviver, trabalhou na Companhia Dramática Julieta dos Santos e, mais tarde, na Estrada de Ferro Central do Brasil,
