8bfed5092ce7298bba4bdbdacbd2ab59bd4ea309
Vitor Prates <103116523+arthurmdondoni@users.noreply.github.com>
2022-06-27 08:10:50 -0300

    .

---
+++ Site quase final/writtersPage.php
@@ -0,0 +1,95 @@
+<?php
+
+//includes com conexão com banco de dados (BD) - no arquivo conn.php -,a head do do HTML - no arquivo
+//head.php - e o conteúdo do cabeçalho na tag header - no arquivo header.php.
+//Todos na pasta de includes.
+include_once 'includes/head.php';
+include_once 'includes/header.php';
+include_once "includes/conn.php";
+
+?>
+
+
+<br>
+<br>
+<br>
+<br>
+
+
+
+<body>
+ <!--Sobre-->
+ <div class="nomeautor">
+	<h3>nomeautor</h3>
+
+
+ </div>
+ <div class="nomeautor2">
+	 <h4>, o esciorot romantica</h4>
+
+
+ </div>
+<div class="container">
+  <div class="row align-items-center">
+
+  <div class="col">
+  <img src="images/logo.png" width=200px height=200px>
+  </div>
+
+    <div class="col">
+<div class= "wittertxt">
+    <h2></h2>
+<h5>(1861-1891)</h5>
+
+<p>Cruz e Sousa, principal nome do Simbolismo brasileiro, nasceu em 24 de novembro de 1861. Filho de escravos alforriados, teve acesso à educação formal graças ao apadrinhamento do ex-“dono” de sua mãe. Durante toda a sua vida, sofreu com o preconceito racial, mas lutou contra a escravidão e o racismo. Para sobreviver, trabalhou na Companhia Dramática Julieta dos Santos e, mais tarde, na Estrada de Ferro Central do Brasil,
+   quando adquiriu tuberculose, o que o levou à morte em 19 de março de 1898.</p>
+</div>
+    </div>
+  </div>
+</div>
+
+<div class="destaques">
+	<h4>Maiores destaques:</h4>
+
+
+</div>
+
+<!--Fim sobre-->
+
+        <div class="txt7">
+		<img style=" width: 300px; height: 150px; margin-top: 15px;margin-bottom: 15px;margin-left: 35px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcGN55Coi1qOh6xqihjhpl9OXaX3YCZBA1ew&usqp=CAU" alt="..." class="img-thumbnail">
+		<div class="p">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec eros purus. Aenean congue sem sit amet lacus blandit ultricies. Ut fermentum, dolor vel sagittis accumsan, ex orci aliquet risus, quis semper orci eros eu orci. Vestibulum a sapien vestibulum, fringilla mauris vitae, ultricies nunc. Cras id tempus nibh, sit amet vehicula justo. Curabitur quis vehicula neque. Quisque ac dui at tellus maximus cursus. Donec sed felis laoreet, tristique neque a, scelerisque velit.	<button class="btn1" href="writtersPage.php" class="BUTTON_VGE">Baixe Já!</button></p>
+		</div>
+        </div>
+		<div class="txt7">
+		<img style=" width: 300px; height: 150px; margin-top: 15px;margin-bottom: 15px;margin-left: 35px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcGN55Coi1qOh6xqihjhpl9OXaX3YCZBA1ew&usqp=CAU" alt="..." class="img-thumbnail">
+		<div class="p">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec eros purus. Aenean congue sem sit amet lacus blandit ultricies. Ut fermentum, dolor vel sagittis accumsan, ex orci aliquet risus, quis semper orci eros eu orci. Vestibulum a sapien vestibulum, fringilla mauris vitae, ultricies nunc. Cras id tempus nibh, sit amet vehicula justo. Curabitur quis vehicula neque. Quisque ac dui at tellus maximus cursus. Donec sed felis laoreet, tristique neque a, scelerisque velit.	<button class="btn1" href="writtersPage.php" class="BUTTON_VGE">Baixe Já!</button></p>
+		</div>
+        </div>
+		<div class="txt7">
+		<img style=" width: 300px; height: 150px; margin-top: 15px;margin-bottom: 15px;margin-left: 35px;" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRcGN55Coi1qOh6xqihjhpl9OXaX3YCZBA1ew&usqp=CAU" alt="..." class="img-thumbnail">
+		<div class="p">
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nec eros purus. Aenean congue sem sit amet lacus blandit ultricies. Ut fermentum, dolor vel sagittis accumsan, ex orci aliquet risus, quis semper orci eros eu orci. Vestibulum a sapien vestibulum, fringilla mauris vitae, ultricies nunc. Cras id tempus nibh, sit amet vehicula justo. Curabitur quis vehicula neque. Quisque ac dui at tellus maximus cursus. Donec sed felis laoreet, tristique neque a, scelerisque velit.	<button class="btn1" href="writtersPage.php" class="BUTTON_VGE">Baixe Já!</button></p>
+		</div>
+        </div>
+    
+
+
+
+
+
+
+
+<!-- Bootstrap-->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+
+<?php
+
+include_once 'includes/footer.php';
+
+?>
